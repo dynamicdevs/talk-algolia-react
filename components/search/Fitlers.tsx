9d671fbb358b0e3c4f1b1b3896bad3b240b5a81c
cnerylozada <cnery@dynamicdevs.io>
2022-06-04 16:32:26 -0500

    feat: ClearRefinements

--- components/search/Fitlers.tsx
+++ components/search/Fitlers.tsx
@@ -1,9 +1,11 @@
+import { ClearRefinements } from "react-instantsearch-dom";
 import { SelectRefinementList } from "./SelectRefinementList";
 
 export const Filters = () => {
   return (
     <>
       <div className="font-bold">Filters</div>
+      <ClearRefinements />
       <SelectRefinementList label="Genres" attribute="genres" />
       <SelectRefinementList label="Players" attribute="players" />
       <SelectRefinementList label="Platforms" attribute="platforms" />
