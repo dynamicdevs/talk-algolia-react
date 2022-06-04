eee2fa997c1a3aa5ef61ef5a47bfb77d24cc0657
cnerylozada <cnery@dynamicdevs.io>
2022-06-04 12:43:44 -0500

    feat: basic InstantSearch

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,8 +1,28 @@
+import {
+  InstantSearch,
+  SearchBox,
+  Hits,
+  Highlight,
+} from "react-instantsearch-dom";
+import { instantMeiliSearch } from "@meilisearch/instant-meilisearch";
+
+const searchClient = instantMeiliSearch(
+  `${process.env.NEXT_PUBLIC_MEILISEARCH_BASEURL}`,
+  `${process.env.NEXT_PUBLIC_MEILISEARCH_API_KEY}`
+);
+const Hit = ({ hit }: { hit: any }) => <Highlight attribute="name" hit={hit} />;
+
 const Search = () => {
   return (
     <>
       <div>
-        <div>Search</div>
+        <InstantSearch
+          indexName="steam-video-games"
+          searchClient={searchClient}
+        >
+          <SearchBox />
+          <Hits hitComponent={Hit} />
+        </InstantSearch>
       </div>
     </>
   );
