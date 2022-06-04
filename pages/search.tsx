03c782f8292e9191114f962111bd74da834b3fe6
cnerylozada <cnery@dynamicdevs.io>
2022-06-04 15:19:26 -0500

    feat: basic filters

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,4 +1,8 @@
-import { InstantSearch, SearchBox } from "react-instantsearch-dom";
+import {
+  InstantSearch,
+  SearchBox,
+  RefinementList,
+} from "react-instantsearch-dom";
 import { instantMeiliSearch } from "@meilisearch/instant-meilisearch";
 import { CustomHits } from "@/components";
 
@@ -16,8 +20,24 @@ const Search = () => {
           searchClient={searchClient}
         >
           <SearchBox />
-          <div className="grid gap-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-            <CustomHits />
+          <div className="flex space-x-5">
+            <div className="flex-shrink-0 w-80 space-y-5">
+              <div>
+                <h2 className="font-bold text-dy-blueishgrey">Genres</h2>
+                <RefinementList attribute="genres" />
+              </div>
+              <div>
+                <h2 className="font-bold text-dy-blueishgrey">Players</h2>
+                <RefinementList attribute="players" />
+              </div>
+              <div>
+                <h2 className="font-bold text-dy-blueishgrey">Platforms</h2>
+                <RefinementList attribute="platforms" />
+              </div>
+            </div>
+            <div className="grid gap-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+              <CustomHits />
+            </div>
           </div>
         </InstantSearch>
       </div>
