bbbf6636c47b53ef8d34de27c734f4f58875676e
cnerylozada <cnery@dynamicdevs.io>
2022-06-04 16:55:25 -0500

    feat: CustomClearRefinements

--- components/search/Fitlers.tsx
+++ components/search/Fitlers.tsx
@@ -1,11 +1,27 @@
-import { ClearRefinements } from "react-instantsearch-dom";
+import { connectCurrentRefinements } from "react-instantsearch-dom";
 import { SelectRefinementList } from "./SelectRefinementList";
+
+const ClearRefinements = ({ items, refine }: { items: any; refine: any }) => (
+  <button
+    onClick={() => refine(items)}
+    disabled={!items.length}
+    className={`py-2 px-4 border rounded-md capitalize ${
+      !items.length
+        ? "cursor-not-allowed bg-dy-gray-1"
+        : "border-dy-purple text-dy-purple hover:bg-dy-purple hover:text-white"
+    }`}
+  >
+    Clear all refinements
+  </button>
+);
+
+const CustomClearRefinements = connectCurrentRefinements(ClearRefinements);
 
 export const Filters = () => {
   return (
     <>
       <div className="font-bold">Filters</div>
-      <ClearRefinements />
+      <CustomClearRefinements />
       <SelectRefinementList label="Genres" attribute="genres" />
       <SelectRefinementList label="Players" attribute="players" />
       <SelectRefinementList label="Platforms" attribute="platforms" />
